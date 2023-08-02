bb90167eb30f589115385a70cc2dd642cb8af08e
codeoz <codeoz@proton.me>
2023-08-03 01:34:11 +0300

    task-2 finish

---
+++ NodeJS/ODEVLER/task-2/app.js
@@ -0,0 +1,30 @@
+const posts = [
+    { postName: 'Post 1', postContent: 'Post 1 Content' },
+    { postName: 'Post 2', postContent: 'Post 1 Content' },
+    { postName: 'Post 3', postContent: 'Post 1 Content' },
+]
+
+const listPosts = () => {
+    posts.map((post) => {
+        console.log(post.postName)
+    })
+}
+
+const addPost = (newPost) => {
+    return new Promise((resolve, reject) => {
+        posts.push(newPost)
+        resolve(posts)
+        //reject("Bir hata oluştu")
+    })
+}
+
+async function showPosts() {
+    try {
+        await addPost({ postName: 'Post 4', postContent: 'Post 4 Content' })
+        listPosts()
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+showPosts()
