a44ef1d75c2a7321878055f78fd7d20a95476b10
codeoz <codeoz@proton.me>
2023-08-03 23:32:54 +0300

    middleware ile html sayfasi gosterimi

--- patika/NodeJS/PCAT/app.js
+++ patika/NodeJS/PCAT/app.js
@@ -1,24 +1,19 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
-app.use(express.static('public'))
 
-const myLogger = (req, res, next) => {
-    console.log('Middleware Log 1')
-}
+//MIDDLEWARES
+app.use(express.static('public')) //statik dosyalarimizi public klasoru altinda tutuyoruz diye belirtiyoruz
+
 
 app.get('/', (req, res) => {
-    app.use(myLogger)
-
-    const photo = {
-        id: 1,
-        name: 'Photo Name',
-        description: 'Photo description',
-    }
-
-    res.send(photo)
+    //path.resolve() fonksiyonu ile dosya yolunu belirtiyoruz
+     res.sendFile(path.resolve(__dirname, 'temp/index.html'))
 })
+
+console.log(__dirname);
 
 const port = 3000
 
@@ -26,10 +21,3 @@ app.listen(port, () => {
     console.log(`Sunucu port ${port} de başlatildi`)
 })
 
-//static dosyalarimizi public klasoru altinda tutuyoruz
-//static: statik dosyanin icerisinde html, css, js, img dosyalari olabilir
-//middleware: uygulama ile sunucu arasinda bir nevi arac gorevi gorur. req-res dongusu icersindeki her seye middleware denir. req-res ile alakali herseyi middleware kullanarak yapabiliriz
-//middleware fonksiyonlari uc parametre alir: req, res, next
-//next: middleware fonksiyonunun bittigini belirtir. next() yazilmazsa middleware fonksiyonu sonsuza kadar calisir
-//middleware fonksiyonlari calistirmak icin app.use() fonksiyonu kullanilir
-//logger => kaydedici
