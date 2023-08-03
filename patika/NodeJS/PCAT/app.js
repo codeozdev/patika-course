5d1dfe90cf2d9c6fe66aee2a16574d3cbb188db7
codeoz <codeoz@proton.me>
2023-08-03 23:34:17 +0300

    middleware ile html sayfasi gosterimi

--- patika/NodeJS/PCAT/app.js
+++ patika/NodeJS/PCAT/app.js
@@ -3,21 +3,19 @@ const path = require('path')
 
 const app = express()
 
-
 //MIDDLEWARES
 app.use(express.static('public')) //statik dosyalarimizi public klasoru altinda tutuyoruz diye belirtiyoruz
 
-
 app.get('/', (req, res) => {
     //path.resolve() fonksiyonu ile dosya yolunu belirtiyoruz
-     res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+    res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+    //__dirname ile dosyanin bulundugu klasoru belirtiyoruz ve ardindan html'nin dosya yolunu belirtiyoruz
 })
 
-console.log(__dirname);
+console.log(__dirname)
 
 const port = 3000
 
 app.listen(port, () => {
     console.log(`Sunucu port ${port} de başlatildi`)
 })
-
