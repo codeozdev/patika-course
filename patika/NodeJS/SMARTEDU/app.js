4ed8dea0b7b72ad80542df4d294e22924d9a7085
codeoz <codeoz@proton.me>
2023-08-07 23:24:15 +0300

    Lesson 02

--- patika/NodeJS/SMARTEDU/app.js
+++ patika/NodeJS/SMARTEDU/app.js
@@ -8,13 +8,21 @@ app.set('view engine', 'ejs')
 //Middlewares
 app.use(express.static('public'))
 
-
+//ROUTES
 app.get('/', (req, res) => {
-    res.status(200).send('INDEX SAYFASI')
+  res.status(200).render('index', {
+    page_name: 'index', //page_name adinda bir degisken olusturduk ve bunu index.ejs dosyasinda kullanacagiz
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.status(200).render('about', {
+    page_name: 'about',  //page_name adinda bir degisken olusturduk ve bunu about.ejs dosyasinda kullanacagiz
+  })
 })
 
 const port = 3000
 
 app.listen(port, () => {
-    console.log(`Sunucu port ${port} de başlatildi`)
+  console.log(`Sunucu port ${port} de başlatildi`)
 })
